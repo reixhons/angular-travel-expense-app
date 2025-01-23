536bbbe409b86e54333b3a24369e5bf183fc131c
reixhons <118919583+reixhons@users.noreply.github.com>
2025-01-23 15:57:47 +0100

    VO.2 - Added route components

---
+++ src/app/routes/trip-detail/trip-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-detail',
+  imports: [],
+  templateUrl: './trip-detail.component.html',
+  styleUrl: './trip-detail.component.scss'
+})
+export class TripDetailComponent {
+
+}
